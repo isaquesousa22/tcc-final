455887f131446ff5d599c031566a945b74174cb8
isaque <isaqueluzsousa83@gmail.com>
2025-08-13 21:55:53 -0300

    up

---
+++ app/components/cards.tsx
@@ -0,0 +1,9 @@
+export function Card() {
+    return (
+        <div>
+            <button className="absolute p-5 rounded-2xl bg-[#118d49] top-[800] left-2/4">começar</button>
+
+            <img className="w-full h-[800px] object-cover object-top" src="https://greenviewgv.com.br/wp-content/uploads/2023/02/352-Lixo-Eletronico-Feed.jpg" alt="" />
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,16 @@
+import { Card } from "./components/cards";
+import { Header } from "./components/header";
+
 export default function Ecopage() {
   return (
     <div>
-      <div className="">
-        <h1>Eco Page </h1>
-        <a>Home</a>
-        <a>Serviços</a>
-        <a>Sobre nos</a>
-      </div>
       <div>
-        <div>
-          <h1>EcoPage</h1>
-          <p>subtitulo</p>
-          <button>Comecar</button>
-        </div>
-        
-      </div>
+        <Header/>
+       </div>
 
-      <div>
-      <h1>Why Recycling Matters</h1>
-      
+      <div className="mt-25">
+    
+       <Card/>
 
       </div>
     </div>
