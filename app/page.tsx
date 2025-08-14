ff4c07668747f6d060e26cbeb8d1e68969dc00fc
isaque <isaqueluzsousa83@gmail.com>
2025-08-13 21:19:22 -0300

    up

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+export default function Ecopage() {
+  return (
+    <div>
+      <div className="">
+        <h1>Eco Page </h1>
+        <a>Home</a>
+        <a>Serviços</a>
+        <a>Sobre nos</a>
+      </div>
+      <div>
+        <div>
+          <h1>EcoPage</h1>
+          <p>subtitulo</p>
+          <button>Comecar</button>
+        </div>
+        
+      </div>
+
+      <div>
+      <h1>Why Recycling Matters</h1>
+      
+
+      </div>
+    </div>
+  )
+}
