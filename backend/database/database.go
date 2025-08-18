3c51a3197eb8c26cd2c13e33d49346c421f7cdaa
gabriel <ga.lima2205@gmail.com>
2025-08-17 21:46:06 -0300

    feat(config): create Dockerfile Backend

--- backend/database/database.go
+++ backend/database/database.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 
 func Connect(){
 	
-	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/reciclagem_db?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:root@tcp(mysql:3306)/reciclagem_db?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 
 	if err != nil {
 		panic("Error ao Conectar no Banco : " + err.Error())
